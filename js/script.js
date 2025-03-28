4a1f7b24a3b4db5225c703b69b460f3086edac01
naivv1 <ivnbdngn5@gmail.com>
2025-03-28 15:17:54 +0800

    added pictures

--- js/script.js
+++ js/script.js
@@ -3,3 +3,25 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+
+function enlargeImage(img) {
+  // Create overlay
+  let overlay = document.createElement("div");
+  overlay.classList.add("enlarged-overlay");
+  overlay.onclick = closeImage;
+
+  // Create enlarged image
+  let enlargedImg = img.cloneNode();
+  enlargedImg.classList.add("enlarged-img");
+  enlargedImg.onclick = closeImage;
+
+  // Append elements to body
+  overlay.appendChild(enlargedImg);
+  document.body.appendChild(overlay);
+}
+
+function closeImage() {
+  document.querySelector(".enlarged-overlay").remove();
+}
+
+
